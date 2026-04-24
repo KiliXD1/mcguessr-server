7c53543587fcd8e992bdf25b7f74eb5bf0254354
KiliXD1 <erik@siegelnet.de>
2026-04-24 19:59:26 +0200

    nichts geändert

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const fs = require("fs");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const FILE = "leaderboard.json";
+
+// GET leaderboard
+app.get("/leaderboard", (req, res) => {
+  const data = JSON.parse(fs.readFileSync(FILE));
+  res.json(data);
+});
+
+// POST score
+app.post("/score", (req, res) => {
+  const { name, score } = req.body;
+
+  let data = JSON.parse(fs.readFileSync(FILE));
+
+  data.push({ name, score });
+
+  data.sort((a, b) => b.score - a.score);
+  data = data.slice(0, 10);
+
+  fs.writeFileSync(FILE, JSON.stringify(data, null, 2));
+
+  res.sendStatus(200);
+});
+
+app.listen(3000, () => console.log("Server läuft"));
